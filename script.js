dd1cfc03b8d47f70125f431f2f36487e862cb329
deepanm87 <deepan.murugan29@gmail.com>
2025-03-04 14:46:14 -0500

    added countdown functionality with set interval

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const timeElements = document.querySelectorAll('span')
 let countdownTitle = ''
 let countdownDate = ''
 let countdownValue = Date
+let countdownActive
 const second = 1000
 const minute = second * 60
 const hour = minute * 60
@@ -18,28 +19,43 @@ const today = new Date().toISOString().split('T')[0]
 dateEl.setAttribute('min', today)
 
 countdownForm.addEventListener('submit', updateCountdown)
+countdownBtn.addEventListener('click', reset)
 
 function updateCountdown(e) {
     e.preventDefault()
     countdownTitle = e.srcElement[0].value
-    countdownDate = e.srcElement[1].value
-    countdownValue = new Date(countdownDate).getTime()
-    updateDOM()
+    if(countdownDate === '') {
+        alert('Please select a date for the countdown')
+    } else {
+        countdownDate = e.srcElement[1].value
+        countdownValue = new Date(countdownDate).getTime()
+        updateDOM()
+    }
 }
 
 function updateDOM() {
-    const now = new Date().getTime()
-    const distance = countdownValue - now
-    const days = Math.floor(distance / day)
-    const hours = Math.floor((distance % day) / hour)
-    const minutes = Math.floor((distance % hour) / minute)
-    const seconds = Math.floor((distance % minute) / second)
-    countdownElTitle.textContent = `${countdownTitle}`
-    timeElements[0].textContent = `${days}`
-    timeElements[1].textContent = `${hours}`
-    timeElements[2].textContent = `${minutes}`
-    timeElements[3].textContent = `${seconds}`
-    inputContainer.hidden = true
-    countdownEl.hidden = false
+    countdownActive = setInterval(() => {
+        const now = new Date().getTime()
+        const distance = countdownValue - now
+        const days = Math.floor(distance / day)
+        const hours = Math.floor((distance % day) / hour)
+        const minutes = Math.floor((distance % hour) / minute)
+        const seconds = Math.floor((distance % minute) / second)
+        countdownElTitle.textContent = `${countdownTitle}`
+        timeElements[0].textContent = `${days}`
+        timeElements[1].textContent = `${hours}`
+        timeElements[2].textContent = `${minutes}`
+        timeElements[3].textContent = `${seconds}`
+        inputContainer.hidden = true
+        countdownEl.hidden = false
+    }, second)
     
+}
+
+function reset() {
+    countdownEl.hidden = true
+    inputContainer.hidden = false
+    clearInterval(countdownActive)
+    countdownTitle = ''
+    countdownDate = ''
 }
